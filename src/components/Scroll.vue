449a0e20b4531178f38d472620316894cae45259
孙珺宇 <sunjy@archempower.com>
2024-04-20 11:24:48 +0800

    模拟数据调试

--- src/components/Scroll.vue
+++ src/components/Scroll.vue
@@ -10,13 +10,115 @@
 </template>
 <script>
 import * as echarts from 'echarts';
-// import moment from 'moment'
+import moment from 'moment'
 export default {
   name: 'echarts_scroll',
   data() {
     return {
       myChart: null,
-      option: {
+      dayList:[],
+      thisYearData: [],
+      lastYearData: [],
+      data:[{
+        date: "2023-01-05",
+        type: "本年度",
+        month: "1 月",
+        value: 42
+      },
+      {
+        date: "2023-02-03",
+        type: "本年度",
+        month: "2 月",
+        value: 67
+      },
+      {
+        date: "2023-03-12",
+        type: "本年度",
+        month: "3 月",
+        value: 38
+      },
+      {
+        date: "2023-04-20",
+        type: "本年度",
+        month: "4 月",
+        value: 55
+      },
+      {
+        date: "2023-05-05",
+        type: "本年度",
+        month: "5 月",
+        value: 76
+      },
+      {
+        date: "2023-06-10",
+        type: "本年度",
+        month: "6 月",
+        value: 23
+      },
+      {
+        date: "2023-07-15",
+        type: "本年度",
+        month: "7 月",
+        value: 88
+      },
+      {
+        date: "2023-08-20",
+        type: "本年度",
+        month: "8 月",
+        value: 44
+      },
+      {
+        date: "2023-09-05",
+        type: "本年度",
+        month: "9 月",
+        value: 33
+      },
+      {
+        date: "2023-10-10",
+        type: "本年度",
+        month: "10 月",
+        value: 66
+      },
+      {
+        date: "2023-11-15",
+        type: "本年度",
+        month: "11 月",
+        value: 55
+      },
+      {
+        date: "2023-12-20",
+        type: "本年度",
+        month: "12 月",
+        value: 99
+      },
+      {
+        date: "2022-05-06",
+        type: "上一年",
+        month: "5 月",
+        value: 56
+      },
+      {
+        date: "2022-06-07",
+        type: "上一年",
+        month: "6 月",
+        value: 85
+      },
+      {
+        date: "2022-07-09",
+        type: "上一年",
+        month: "7 月",
+        value: 45
+      }]
+    }
+  },
+  methods: {
+    init() {
+      this.myChart = echarts.init(this.$refs.view)
+      this.remakeData();
+      this.myChart.setOption(this.makeOption())
+    },
+    makeOption() {
+      const option = {
         xAxis: {
           type: 'time',
         },
@@ -24,90 +126,56 @@ export default {
           type: 'value'
         },
         series: [{
-          data: [
-            ['2024-01-01', 120],
-            ['2024-01-02', 132],
-            ['2024-01-03', 101],
-            ['2024-01-04', 134],
-            ['2024-01-05', 90],
-            ['2024-01-06', 230],
-            ['2024-01-07', 210],
-            ['2024-01-08', 220],
-            ['2024-01-09', 182],
-            ['2024-01-10', 191],
-            ['2024-01-11', 234],
-            ['2024-01-12', 290],
-            ['2024-01-13', 330],
-            ['2024-01-14', 310],
-            ['2024-01-15', 123],
-            ['2024-01-16', 220],
-            ['2024-01-17', 210],
-            ['2024-01-18', 220],
-            ['2024-01-19', 182],
-            ['2024-01-20', 191],
-            ['2024-01-21', 234],
-            ['2024-01-22', 290],
-            ['2024-01-23', 330],
-            ['2024-01-24', 310],
-            ['2024-01-25', 123],
-            ['2024-01-26', 220],
-            ['2024-01-27', 210],
-            ['2024-01-28', 220],
-          ],
+          data: this.thisYearData,
           type: 'line',
           markLine: {
             data: [],
           }
         },{
-          data: [
-            ['2024-01-01', 220],
-            ['2024-01-02', 122],
-            ['2024-01-03', 121],
-            ['2024-01-04', 124],
-            ['2024-01-05', 20],
-            ['2024-01-06', 130],
-            ['2024-01-07', 220],
-            ['2024-01-08', 120],
-            ['2024-01-09', 182],
-            ['2024-01-10', 291],
-            ['2024-01-11', 134],
-            ['2024-01-12', 290],
-            ['2024-01-13', 320],
-            ['2024-01-14', 210],
-            ['2024-01-15', 223],
-            ['2024-01-16', 210],
-            ['2024-01-17', 180],
-            ['2024-01-18', 120],
-            ['2024-01-19', 122],
-            ['2024-01-20', 121],
-            ['2024-01-21', 234],
-            ['2024-01-22', 250],
-            ['2024-01-23', 130],
-            ['2024-01-24', 210],
-            ['2024-01-25', 163],
-            ['2024-01-26', 230],
-            ['2024-01-27', 110],
-            ['2024-01-28', 160],
-          ],
+          data: this.lastYearData,
           type: 'line'
         }],
         dataZoom: [{
           type: 'slider',
-          startValue: '2024-01-01',
-          endValue: '2024-01-08',
+          startValue: this.dataZoomStart,
+          endValue: this.dataZoomEnd,
         },{
           type: 'inside',
-          startValue: '2024-01-01',
-          endValue: '2024-01-08',
+          startValue: this.dataZoomStart,
+          endValue: this.dataZoomEnd,
         }]
-      },
-      dayList:[]
-    }
-  },
-  methods: {
-    init() {
-      this.myChart = echarts.init(this.$refs.view)
-      this.myChart.setOption(this.option)
+      }
+      return option;
+    },
+    formatDate(date) {
+      return `${moment().format('YYYY')}-${moment(date).format('MM-DD')}`;
+    },
+    remakeData() {
+      const {data} = this;
+      const thisYearData = [];
+      const lastYearData = [];
+      const dayList = [];
+      const dataSort = data.sort((a,b) => {
+        return moment(this.formatDate(a.date)).valueOf()-moment(this.formatDate(b.date)).valueOf()
+      })
+      // 将data中数据按照date的月日排序
+
+
+      // 遍历Data数据，根据年度分别按照[date,value]格式放入thisYearData和lastYearData中，并将所有date格式化成今年日期存入dayList中
+      dataSort.forEach(item => {
+        const {date,value} = item;
+        const dateStr = this.formatDate(date)
+        if(item.type === '本年度') {
+          thisYearData.push([dateStr, value]);
+        } else {
+          lastYearData.push([dateStr, value]);
+        }
+        
+        dayList.push(dateStr);
+      })
+      this.$set(this, 'lastYearData', lastYearData);
+      this.$set(this, 'thisYearData', thisYearData);
+      this.$set(this, 'dayList', dayList)
     },
     change(e,item,index) {
       const dom = e.target;
@@ -156,16 +224,8 @@ export default {
           }]
       });
     },
-    initDayList() {
-      const dayList = [];
-      for(let i = 1; i <= 28; i++) {
-        dayList.push(`2024-01-${i < 10 ? '0' + i : i}`);
-      }
-      this.dayList = dayList;
-    }
   },
   mounted() {
-    this.initDayList()
     this.init()
   }
 }
